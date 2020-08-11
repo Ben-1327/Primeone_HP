73b82f42cd1a337de2d20c18d35c8a7ca6c974c7
Ben-1327 <ff.walker1327@gmail.com>
2020-08-11 11:14:34 +0900

    [Fix] admin_table [Add] after_signed_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  protected
+  def after_sign_in_path_for(resource)
+    case resource
+    when AdminUser
+      admin_top_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin_user
+      new_admin_user_session_path
+    end
+  end
+
 end
