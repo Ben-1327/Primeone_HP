34ac9baac8a307d62f07de469a38b3bcb2e1c80a
Ben-1327 <ff.walker1327@gmail.com>
2020-08-12 14:27:19 +0900

    [Add] validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+
+  validates :user_name, presence: true
+  validates :ruby, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
+  validates :tel, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
+  validates :contents, presence: true
+
 end
